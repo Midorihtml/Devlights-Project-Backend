1e9bbde76ba13d41dffedd9bc628afdc1f0b215d
Guillermo Fernandez <fernandez.vga.edu@gmail.com>
2025-11-15 19:12:48 -0300

    ADD (root) se añade linter y se ajustan configuraciones de typescript, ademas de añadirse path para rutas absolutas, se cambian script para dev con tsx, linter, y build con tsc

---
+++ eslint.config.js
@@ -0,0 +1,35 @@
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsparser from "@typescript-eslint/parser";
+import prettier from "eslint-config-prettier";
+import eslintPluginPrettier from "eslint-plugin-prettier";
+
+/** @type {import("eslint").Linter.Config[]} */
+export default [
+  {
+    files: ["src/**/*.{ts,tsx}"],
+    ignores: ["dist/**"],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module"
+      }
+    },
+
+    plugins: {
+      "@typescript-eslint": tseslint,
+      prettier: eslintPluginPrettier
+    },
+
+    rules: {
+      // Reglas de TypeScript recomendadas
+      ...tseslint.configs.recommended.rules,
+
+      // ❗ Deshabilitar cualquier regla que choque con Prettier
+      ...prettier.rules,
+
+      // 👉 Ejecutar Prettier como regla ESLint
+      "prettier/prettier": "error"
+    }
+  }
+];
