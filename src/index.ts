b90409929dffbe61e1d81f7e55a681a6b8eac7ad
Guillermo Fernandez <fernandez.vga.edu@gmail.com>
2025-11-24 16:21:46 -0300

    TEST (vercel)

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import { app } from "@src/app";
 
-const PORT = process.env["PORT"] || 3000;
-const HOST = process.env["HOST"] || "localhost";
-const PROTOCOL = process.env["NODE_ENV"] === "production" ? "https" : "http";
+// const PORT = process.env["PORT"] || 3000;
+// const HOST = process.env["HOST"] || "localhost";
+// const PROTOCOL = process.env["NODE_ENV"] === "production" ? "https" : "http";
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server listening: ${PROTOCOL}://${HOST}:${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`🚀 Server listening: ${PROTOCOL}://${HOST}:${PORT}`);
+// });
 
 export default app;
